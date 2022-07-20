2ebde2652f06f25c9fd854af2437460aaf3681eb
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-07-20 13:09:33 +0100

    Uncompleted MyDose

--- app/src/main/java/com/example/myapplication/MyDose.kt
+++ app/src/main/java/com/example/myapplication/MyDose.kt
@@ -1,10 +1,16 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -19,6 +25,19 @@ fun MyDose(){
             val navController = rememberNavController()
             val doseTopLevelNavigation = rememberNavController(){
 
+            }
+            val navBackStack by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStack?.destination
+            
+            val fabVisibility = rememberSaveable {
+                (mutableStateOf(true))
+            }
+            val bottomNavVisibility = rememberSaveable {
+                (mutableStateOf(true))
+            }
+            
+            Scaffold() {
+                
             }
         }
     }
