65b922af63e47a54910a896488402d20d96f322c
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-07-22 01:55:28 +0100

    Rectify few bugs

--- app/src/main/java/com/example/myapplication/MyDose.kt
+++ app/src/main/java/com/example/myapplication/MyDose.kt
@@ -1,97 +1,98 @@
 package com.example.myapplication
 
-import android.view.WindowInsets
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+@OptIn(ExperimentalLayoutApi::class)
+@SuppressLint("RememberReturnType")
 @Composable
 fun MyDose() {
-//    MyApplicationTheme {
-//        // A surface container using the 'background' color from the theme
-//        Surface(
-//            modifier = Modifier.fillMaxSize(),
-//            color = MaterialTheme.colorScheme.background
-//        ) {
-//
-//            val navController = rememberNavController()
-//            val doseTopLevelNavigation = remember(navController) {
-//                DoseTopLevelNavigation(navController)
-//            }
-//
-//            val navBackStackEntry by navController.currentBackStackEntryAsState()
-//            val currentDestination = navBackStackEntry?.destination
-//
-//            val bottomBarVisibility = rememberSaveable { (mutableStateOf(true)) }
-//            val fabVisibility = rememberSaveable { (mutableStateOf(true)) }
-//
-//            Scaffold(
-//                modifier = Modifier.padding(24.dp),
-//                containerColor = Color.Transparent,
-//                contentColor = MaterialTheme.colorScheme.onBackground,
-//                floatingActionButton = {
-//                    AnimatedVisibility(
-//                        visible = fabVisibility.value,
-//                        enter = slideInVertically(initialOffsetY = { it }),
-//                        exit = slideOutVertically(targetOffsetY = { it }),
-//                        content = {
-//                            DoseFAB(navController)
-//                        })
-//
-//                },
-//                bottomBar = {
-//                    AnimatedVisibility(
-//                        visible = bottomBarVisibility.value,
-//                        enter = slideInVertically(initialOffsetY = { it }),
-//                        exit = slideOutVertically(targetOffsetY = { it }),
-//                        content = {
-//                            DoseBottomBar(
-//                                onNavigateToTopLevelDestination = doseTopLevelNavigation::navigateTo,
-//                                currentDestination = currentDestination
-//                            )
-//                        })
-//                }
-//            ) { padding ->
-//                Row(
-//                    Modifier
-//                        .fillMaxSize()
-//                        .windowInsetsPadding(
-//                            WindowInsets.safeDrawing.only(
-//                                WindowInsetsSides.Horizontal
-//                            )
-//                        )
-//                ) {
-//
-//                    DoseNavHost(
-//                        bottomBarVisibility = bottomBarVisibility,
-//                        fabVisibility = fabVisibility,
-//                        navController = navController,
-//                        modifier = Modifier
-//                            .padding(padding)
-//                            .consumedWindowInsets(padding)
-//                    )
-//                }
-//            }
-//
-//        }
-//    }
+    MyApplicationTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+
+            val navController = rememberNavController()
+            val doseTopLevelNavigation = remember(navController) {
+                DoseTopLevelNavigation(navController)
+            }
+
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+
+            val bottomBarVisibility = rememberSaveable { (mutableStateOf(true)) }
+            val fabVisibility = rememberSaveable { (mutableStateOf(true)) }
+
+            Scaffold(
+                modifier = Modifier.padding(24.dp),
+                containerColor = Color.Transparent,
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                floatingActionButton = {
+                    AnimatedVisibility(
+                        visible = fabVisibility.value,
+                        enter = slideInVertically(initialOffsetY = { it }),
+                        exit = slideOutVertically(targetOffsetY = { it }),
+                        content = {
+                            DoseFAB(navController)
+                        })
+
+                },
+                bottomBar = {
+                    AnimatedVisibility(
+                        visible = bottomBarVisibility.value,
+                        enter = slideInVertically(initialOffsetY = { it }),
+                        exit = slideOutVertically(targetOffsetY = { it }),
+                        content = {
+                            DoseBottomBar(
+                                onNavigateToTopLevelDestination = doseTopLevelNavigation::navigateTo,
+                                currentDestination = currentDestination
+                            )
+                        })
+                }
+            ) { padding ->
+                Row(
+                    Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(
+                            WindowInsets.safeDrawing.only(
+                                WindowInsetsSides.Horizontal
+                            )
+                        )
+                ) {
+
+                    DoseNavHost(
+                        bottomBarVisibility = bottomBarVisibility,
+                        fabVisibility = fabVisibility,
+                        navController = navController,
+                        modifier = Modifier
+                            .padding(padding)
+                            .consumedWindowInsets(padding)
+                    )
+                }
+            }
+
+        }
+    }
 }
 
 @Preview
--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -11,7 +11,9 @@ val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     button = TextStyle(
