5fc4795910e4f592a430e9c4a02e1853f574a769
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-08-14 18:37:58 +0100

    Successfully ran

--- app/src/main/java/com/example/myapplication/ui/theme/Shape.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Shape.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.material3.Shapes
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
-val Shapes = (
+val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
